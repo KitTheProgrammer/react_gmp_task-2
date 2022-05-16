6145d165be52b19caf4bfbc659bcb9846975e87c
Anton Fan <anton.fan@cantire.com>
2022-05-16 16:35:19 +0300

    added optimization for images, styles and code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+const { extendDefaultPlugins } = require('svgo')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = (env) => {
   console.log('Current environment: ', env.NODE_ENV)
@@ -16,7 +20,7 @@ module.exports = (env) => {
         {
           test: /\.s[ac]ss$/i,
           use: [
-            'style-loader',
+            MiniCssExtractPlugin.loader,
             'css-loader',
             'sass-loader',
           ],
@@ -34,6 +38,52 @@ module.exports = (env) => {
       new HtmlWebpackPlugin({
         template: './src/index.html',
       }),
+      new MiniCssExtractPlugin(),
     ],
+    optimization: {
+      mangleWasmImports: true,
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minSize: 0,
+            minChunks: 2,
+          }
+        },
+      },
+      minimizer: [
+        new CssMinimizerPlugin(),
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [
+                ['gifsicle', { interlaced: true }],
+                ['jpegtran', { progressive: true }],
+                ['optipng', { optimizationLevel: 5 }],
+                [
+                  'svgo',
+                  {
+                    plugins: extendDefaultPlugins([
+                      {
+                        name: 'removeViewBox',
+                        active: false,
+                      },
+                      {
+                        name: 'addAttributesToSVGElement',
+                        params: {
+                          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                        },
+                      },
+                    ]),
+                  },
+                ],
+              ],
+            },
+          },
+        }),
+      ],
+    },
   }
 }
